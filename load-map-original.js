246573b950154b1854fa743dfb0d5a162e8dc65a
PoseidonCHNZ <PoseidonCHNZ@gmail.com>
2020-07-30 16:26:33 +1200

    Add files via upload

---
+++ load-map-original.js
@@ -0,0 +1,13 @@
+
+window.onload = function(){
+   var link = "https://poseidonchnz.github.io/weatherSen/index.html" 
+var iframe = document.createElement('iframe');
+iframe.frameBorder=0;
+iframe.width='100%';
+iframe.height='500px';
+iframe.id="randomid";
+iframe.setAttribute("src", link);
+document.getElementById("ad54").appendChild(iframe);
+}
+
+
